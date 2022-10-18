530f4cda437e577339d83fbdb5324968d61e0b0b
Dmitry <dep1982@mail.ru>
2022-10-18 19:13:34 +0300

    Zadacha64

--- DZ64/Program.cs
+++ DZ64/Program.cs
@@ -1,34 +1,12 @@
 ﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
-int InNumber()
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetNaturalNumbers(N));
+
+string GetNaturalNumbers(int N)
 {
-    while (true)
-    {
-        string text = Console.ReadLine();
-        if (int.TryParse(text, out int number) && number > 0)
-        {
-            return number;
-        }
-        else
-        {
-            Console.Write("Не удалось распознать число, либо оно отрицательное. \nВведите число ещё раз: ");
-        }
-    }
-}
-
-Console.Write("Введите положительное число: ");
-int number = InNumber();
-
-NaturalNumber(number);
-
-void NaturalNumber(int number)
-{
-    if (number == 0)
-    {
-        return;
-    }
-    else
-    {
-        Console.Write($"{number} ");
-        NaturalNumber(number - 1);
-    }
+    if (N == 1)
+        return "1";
+    return N + ", " + GetNaturalNumbers(N - 1);
 }
